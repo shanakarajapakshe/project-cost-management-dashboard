d88b7f7d37bc3668a487dab488f82a41aa440028
shanakarajapakshe <rajapaksheshanaka@gmail.com>
2026-03-13 16:06:09 +0530

    Replace with fixed version - all bugs resolved + auto-update

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow() {
             contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
         },
-        icon: path.join(__dirname, 'icon.png'),
+        icon: path.join(__dirname, 'assets/icon.ico'),
         title: 'Profit Dashboard',
         backgroundColor: '#f5f7fa'
     });
